e0e1d324a0eb6faa26816a3cd284385d2402ea82
Om Swaroop Nayak <146124307+saeitoshi-10@users.noreply.github.com>
2025-07-17 23:42:05 +0530

    Updated index.css

--- client/src/components/CustomDialog.js
+++ client/src/components/CustomDialog.js
@@ -1,1 +1,25 @@
-// This file is no longer needed - replaced by Modal.js
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+
+export default function CustomDialog({ open, children, title, contentText, handleContinue }) {
+  return (
+    <Dialog open={open}> {}
+      <DialogTitle>{title}</DialogTitle>
+      <DialogContent> {}
+        <DialogContentText> {}
+          {contentText}
+        </DialogContentText>
+        {children} {}
+      </DialogContent>
+      <DialogActions> {}
+        {}
+        {}
+        <Button onClick={handleContinue}>Continue</Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
